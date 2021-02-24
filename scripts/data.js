7eb364288ec96e525be38a1a2d7fa5adbd37d144
joaophgaspar <joaopedrohenriquesgaspar@gmail.com>
2021-02-24 11:42:03 +0000

    language-icons

--- scripts/data.js
+++ scripts/data.js
@@ -18,7 +18,7 @@ const texts = {
         description: "Se estás a ler isto é porque provavelmente me perdi e não sei o caminho para casa. Sou de Fátima mas neste momento estou a estudar em Lisboa. Já aprendi a sentar, a dar a pata, a deitar, a dar a voltinha e a fazer (quase) sempre as necessidades na rua.<br>Agradecia, muito, que contactasses os meus pais, o quanto antes, que eles já devem estar preocupados. O jantar deve estar quase pronto e tenho de comer e ir dormir, que amanhã tenho aulas bem cedinho.<br><br><b>Obrigado por me encontrares</b> 🐶"
     },
     "en": {
-        title: "Hello!<br>Eu sou o Oliver,<br>mas podes chamar-me Oli.",
-        description: "english text</b> 🐶"
+        title: "Hello!<br>I'm Oliver,<br>but you can call me Oli.",
+        description: "If you are reading this it is probably I got lost and I don't know the way home. I'm from Fatima but at the moment I'm studying in Lisbon. I have already learned to sit, to give the paw, to lie down, to spin and to (almost) always defecate in the street.<br>I would really appreciate it if you could contact my parents, as soon as possible, that they should be worried. Dinner should be ready soon and I have to eat and go to bed, since I have early classes tomorrow.<br><br><b>Thanks for finding me</b> 🐶"
     },
 }
